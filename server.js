c1256c4eea144de63a3a8f20c6c365a431a3ea12
batoolalk <btlsharaf@gmail.com>
2025-09-03 19:42:47 +0300

    mongodb connect

--- server.js
+++ server.js
@@ -2,8 +2,17 @@
 // We begin by loading Express
 const express = require('express');
 const PORT = 3000; // should be in env
-
+const morgan = require('morgan');
 const app = express();
+
+//middleware
+app.use(morgan('dev'));
+
+//Routes
+app.get('/', (req, res) => {
+    res.render('index.ejs');
+})
+
 
 app.listen(PORT, () => {
   console.log('Listening on port 3000');
